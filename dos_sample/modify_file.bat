76e4ee51217edc3a27da6aeb5f8ab91f02b436ae
yanrk <yanrkchina@163.com>
2021-06-17 21:27:09 +0800

    add more samples

--- dos_sample/modify_file.bat
+++ dos_sample/modify_file.bat
@@ -1,3 +1,4 @@
 @echo off
+
 powershell -command "(type c:\src.bat) -replace 'template.exe', 'app.exe' | out-file -encoding ascii c:\dst.bat"
-powershell -command "(gc c:\src.txt) -replace 'XXXXXX', $env:COMPUTERNAME | out-file -encoding ASCII c:\dst.txt"
+powershell -command "(gc c:\src.txt) -replace 'xxxxxx', $env:computername | out-file -encoding ascii c:\dst.txt"
