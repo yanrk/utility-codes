df32cf11e7aaaf645ef326584124a104b4fe3c42
yanrk <yanrkchina@163.com>
2023-09-30 22:10:52 +0800

    linux network check

---
+++ network_check/network_check.h
@@ -0,0 +1,17 @@
+#pragma once
+
+enum network_check_result_t
+{
+    network_check_pending,
+    network_check_success,
+    network_check_failure,
+    network_check_timeout,
+    network_check_exception
+};
+
+int get_address(char host_ip[16], char host_mac[18], char host_mask[16], int timeout/* = 2*/);
+int get_gateway(char gateway_ip[16], int timeout/* = 2*/);
+int get_dns(char dns_ip[16], int timeout/* = 2*/);
+int ping_server(const char * ping_ip, int * loss, double * delay, int timeout/* = 6*/);
+int parse_domain(int timeout/* = 2*/);
+int test_download(int * speed, int timeout/* = 30*/);
